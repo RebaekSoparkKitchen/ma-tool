6ed71822a055401c74615f204b5f770d38d98ce9
Flying_Red_Pig <xuzinan2@sina.com>
2020-06-05 09:41:29 +0800

    加入transfer指令来call数据转换interface

--- bin/EDM.py
+++ bin/EDM.py
@@ -3,7 +3,7 @@
 @Author: FlyingRedPig
 @Date: 2020-05-12 19:44:54
 @LastEditors: FlyingRedPig
-@LastEditTime: 2020-05-13 20:05:53
+@LastEditTime: 2020-06-04 19:12:06
 @FilePath: \EDM\bin\EDM.py
 '''
 import fire
@@ -18,6 +18,7 @@ from edm.Spider.BasicPerformance import BasicPerformance
 from edm.Spider.ClickPerformance import ClickPerformance
 from edm.LocalDataBase.LocalData import LocalData
 from edm.Report.ReportExcel import ReportExcel
+from edm.Transfer.gui import DataTransfer
 
 from tabulate import tabulate
 import pandas as pd
@@ -30,12 +31,12 @@ class EDM(object):
 
     def __getTrackerInput(self):
         return self.trackerInput
-    
-    def simple_tracker(self, path:str = "../../files/"):
+
+    def simple_tracker(self, path: str = "../../files/"):
         s = SimpleTracker(path)
         s.simpleTrackerExcel()
         print('Simple_tracker.xlsx 已经创建成功！')
-        return 
+        return
 
     def __pretty(self, df):
         return tabulate(df, headers='keys', tablefmt='psql')
@@ -51,7 +52,7 @@ class EDM(object):
         else:
             print(self.__pretty(a.futureWork()))
         print('\n')
-        
+
         print('待定的工作：')
         if a.waitWork().empty:
             print('\n我们没有待定的需求')
@@ -65,7 +66,7 @@ class EDM(object):
         else:
             print(self.__pretty(a.report()))
         print('\n')
-        
+
         print('我们需要检查的campaign:')
         if a.check().empty:
             print('\n过去的campaign都填上了campaign id, 很棒！')
@@ -78,20 +79,20 @@ class EDM(object):
             print('\n我们没有时间上的避让考虑')
         else:
             print(self.__pretty(a.communicationLimitHint()))
-        
+
         return
 
     def write_campaign_id(self):
-        
+
         a = Analytics()
-        
+
         if a.check().empty:
             return "没啥campaign id需要写啊~"
         for index, row in a.check().iterrows():
             print(row)
             while True:
                 campaignId = input('请输入以上campaign的id，谢谢~')
-                if campaignId =="next" or "exit":
+                if campaignId == "next" or "exit":
                     break
                 try:
                     int(campaignId)
@@ -103,14 +104,16 @@ class EDM(object):
                 break
             elif campaignId == "next":
                 continue
-            else: 
+            else:
                 self.trackerInput.writeCampaignId(index, campaignId)
                 self.trackerInput.saveTracker()
         return
-    
+
     def report(self, campaignId, catagory="static", path='../../report/'):
-        
-        l = LocalData(dataPath="../data/campaign_data.json") #此处硬编码了地址，因为脚本文件和类文件不在同一个位置，那么与campaign_data.json的相对位置也不同
+
+        l = LocalData(
+            dataPath="../data/campaign_data.json"
+        )  #此处硬编码了地址，因为脚本文件和类文件不在同一个位置，那么与campaign_data.json的相对位置也不同
         w = WriteTracker()
         if catagory == "static":
             l.request(False, campaignId)
@@ -118,30 +121,39 @@ class EDM(object):
             l.request(True, campaignId)
         else:
             raise ValueError('catagory参数只接收static或者dynamic')
-        
-        r = ReportExcel(campaignId, templatePath="../config/Report_template.xlsx")
+
+        r = ReportExcel(
+            campaignId, templatePath="../config/Report_template.xlsx")
         r.create(path=path)
         self.trackerInput.writePerformanceData(campaignId)
         self.trackerInput.saveTracker()
         a = Analytics(campaignId)
         print('《{}》数据报告已创建成功！'.format(a.name()))
-        return 
-    
+        return
+
     def routine(self):
-        
+
         a = Analytics()
 
         self.simple_tracker()
         self.workflow()
         self.write_campaign_id()
         reportList = list(a.report()['Campaign ID'])
-        reportList = list(map(int, reportList)) #此行重要，df中campaign_id是float形式
-       
+        reportList = list(map(int, reportList))  #此行重要，df中campaign_id是float形式
+
         for campaignId in reportList:
             self.report(campaignId)
+        return
+
+    def transfer(self):
+        '''
+        弹出数据格式转换的gui
+        '''
+        t = DataTransfer()
+        t.execute()
         return 
-        
+
 
 if __name__ == "__main__":
     fire.Fire(EDM)
-    
+   
---
+++ edm/Transfer/gui.py
@@ -0,0 +1,78 @@
+'''
+@Description: 
+@Author: FlyingRedPig
+@Date: 2020-05-12 13:40:05
+@LastEditors: FlyingRedPig
+@LastEditTime: 2020-06-04 19:17:51
+@FilePath: \EDM\edm\Transfer\gui.py
+'''
+import tkinter as tk
+import edm.Transfer.transfer_csv_to_excel as ce
+from tkinter import filedialog
+
+class DataTransfer(object):
+
+    def __init__(self):
+        
+        self.root = tk.Tk()
+        self.root.title("Data Transfer Tool")
+        self.root.geometry('800x1000')
+
+        self.e1 = tk.Entry(self.root, width=70, show=None, relief='groove')
+        self.e1.insert(0,"C:/Users/C5293427/Desktop/Data_exported/csv_file")
+        self.e1.place(x=160,y=300, anchor='nw')
+
+        self.e2 = tk.Entry(self.root, width=70, show=None)
+        self.e2.insert(0,"C:/Users/C5293427/Desktop/Data_exported/成品")
+        self.e2.place(x=160, y=350, anchor='nw')
+
+        self.l1 = tk.Label(self.root, text='input path', show = None)
+        self.l1.place(x=80,y=300, anchor='nw')
+
+        self.l1 = tk.Label(self.root, text='output path', show = None)
+        self.l1.place(x=78,y=350, anchor='nw')
+
+        self.b1 = tk.Button(self.root,text='transfer',width=15,height=2,command=self.transfer_format)
+        self.b1.place(x=280, y=400, anchor='nw')
+
+        self.b2 = tk.Button(self.root,text='Browse...',width=15,height=1,command=self.select_open_path)   #Browse按钮
+        self.b2.place(x=600, y=298, anchor='nw')
+
+        self.b2 = tk.Button(self.root,text='Save as...',width=15,height=1,command=self.select_save_path)   #Save as按钮
+        self.b2.place(x=600, y=350, anchor='nw')
+        return
+
+    def select_open_path(self):
+        '''
+        为browse按钮写的，删除默认路径，填上选中路径
+        :return:
+        '''
+        file_path = filedialog.askopenfilename(initialdir='C:/Users/C5293427/Desktop/Data_exported/csv_file')
+        self.e1.delete(0, tk.END)
+        self.e1.insert(0, file_path)
+
+    def select_save_path(self):
+        '''
+        为save as按钮写的
+        :return:
+        '''
+        save_path = filedialog.asksaveasfilename(defaultextension='xlsx',initialdir='C:/Users/C5293427/Desktop/Data_exported/成品',filetypes=[('Excel file', '.xlsx'),('all files','.*')])
+        self.e2.delete(0, tk.END)
+        self.e2.insert(0, save_path)
+
+
+    def transfer_format(self):
+
+        input_path = self.e1.get()
+        output_path = self.e2.get()
+
+        file = ce.ExportedData(input_path)
+        file.to_xlsx(output_path)
+        return
+
+    def execute(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    d = DataTransfer()
+    d.execute()
